8f2e2c9240ed0b46b41e5e46524757e23892918f
Florencia Vilchez Acosta <florencia.vilchez@possible.com>
2020-07-17 01:35:45 -0300

    binding some data

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = event => {
+    event.preventDefault();
+    console.log(this.myInput.current.value);
+  };
+
   render() {
     return (
       <>
-        <p>Fish!</p>
-        <form action="" className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
           <h2>Please Enter A Store</h2>
-          <input type="text" required placeholder="Store name" />
+
+          <input type="text" ref={this.myInput} required placeholder="Store name" defaultValue={getFunName()} />
           <button type="submit">Visit Store</button>
         </form>
       </>
