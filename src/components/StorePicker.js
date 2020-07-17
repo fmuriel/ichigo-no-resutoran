14cda4e49da86975ac8ca75092f86505d28efcf4
Florencia Vilchez Acosta <florencia.vilchez@possible.com>
2020-07-17 01:44:11 -0300

    using react router

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -6,7 +6,8 @@ class StorePicker extends React.Component {
 
   goToStore = event => {
     event.preventDefault();
-    console.log(this.myInput.current.value);
+    const storeName = this.myInput.current.value;
+    this.props.history.push(`/store/${storeName}`)
   };
 
   render() {
